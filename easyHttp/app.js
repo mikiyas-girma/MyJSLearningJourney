1dfe9add5cdb8a89dd2c8818517a767c9a239be5
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-08-18 11:37:15 +0300

    make POST  request and created a post

--- easyHttp/app.js
+++ easyHttp/app.js
@@ -14,16 +14,30 @@ const http = new easyHTTP;
 
 // get a single post
 
-http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(post);
-  }
-});
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
 
 
 // create posts
+
+const mypost = {
+  title: " Custom post one",
+  body: "this is custom post one created by post request"
+};
+
+
+http.post('https://jsonplaceholder.typicode.com/posts', mypost, function (err, posts) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
 
 
 // update post
--- easyHttp/easyHttp.js
+++ easyHttp/easyHttp.js
@@ -5,24 +5,38 @@ function easyHTTP() {
 }
 
 // make HTTP GET request
-easyHTTP.prototype.get = function (url, callback) {
+// easyHTTP.prototype.get = function (url, callback) {
   
-  this.htttp.open('GET', url, true);
+//   this.htttp.open('GET', url, true);
   
-  let self = this;
-  this.htttp.onload = function () {
-  if(self.htttp.status === 200) {
-    callback(null, self.htttp.responseText);
-  } else {
-    callback('Error '  + self.htttp.status);
-  }
+//   let self = this;
+//   this.htttp.onload = function () {
+//   if(self.htttp.status === 200) {
+//     callback(null, self.htttp.responseText);
+//   } else {
+//     callback('Error '  + self.htttp.status);
+//   }
 
-  }
+//   }
 
-  this.htttp.send();
-}
+//   this.htttp.send();
+// }
+
 
 // Make an HTTP POST Request
+
+
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.htttp.open('POST', url, true);
+  this.htttp.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.htttp.onload = function () {
+      callback(null, self.htttp.responseText);
+    }
+
+    this.htttp.send(JSON.stringify(data));
+  }
 
 
 
