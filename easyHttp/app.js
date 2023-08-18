3a6c40aa464c9cf796e85ff6dfd472452ef2a076
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-08-18 11:17:16 +0300

    make GET request to retrieve single post

--- easyHttp/app.js
+++ easyHttp/app.js
@@ -3,12 +3,22 @@ const http = new easyHTTP;
 
 // get posts
 
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
 
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// get a single post
+
+http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
   }
 });
 
