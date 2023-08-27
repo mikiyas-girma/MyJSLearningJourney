736d927bf8b39b5f5c3fccd17b6a58de7ecad810
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-08-27 09:34:43 +0300

    added another example for async and await usage

--- async wait/app.js
+++ async wait/app.js
@@ -16,4 +16,30 @@ async function asyncCall() {
   console.log(response);
   }
 
-  asyncCall();
+  // asyncCall();
+
+
+
+  // Another example 
+
+  async function foo() {
+    const p1 = new Promise((resolve) => setTimeout(() => resolve("1"), 1000));
+    const p2 = new Promise((_, reject) => setTimeout(() => reject("2"), 500));
+    
+    // Do not do this! Use Promise.all or Promise.allSettled instead.
+    // const results = [await p1, await p2];
+      // Attempt to swallow all errors... not recommended practice 
+  // foo().catch(() => {}); 
+    try {
+      const results = await Promise.all([p1, p2]);
+      results.forEach((result) => console.log(result) );
+
+    } catch (error) {
+      console.error("An error occurred:", error); // this is expected result since its rejected in p2
+    }
+    
+  }
+
+  
+
+  foo();
