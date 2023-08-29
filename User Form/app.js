d641d35a81d42fbd61813f1bc89b9b77d003e41c
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-08-29 13:15:08 +0300

    validated zip input

--- User Form/app.js
+++ User Form/app.js
@@ -17,4 +17,17 @@ function validateName() {
     name.classList.remove('is-invalid');
   }
 }
+function validateZip() {
+  const zip = document.getElementById('zip');
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+  if(!re.test(zip.value)) {
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+
+}
+
+
+
 
