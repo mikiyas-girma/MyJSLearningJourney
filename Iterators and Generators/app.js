6c7e47f78bf8e2a9027b6042e4864849ab46040a
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-09-11 23:07:59 +0300

    generators

--- Iterators and Generators/app.js
+++ Iterators and Generators/app.js
@@ -18,9 +18,25 @@ const namesArr = ['Jack', 'Jill', 'John'];
 // Init iterator and pass in the names array
 const names = nameIterator(namesArr);
 
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
 
+// genarators
 
+function* createIds() {
+  let index = 1;
+
+  while(true) {
+    yield index++;
+  }
+}
+
+const gen = createIds();
+
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
